099d0abc88d37f981429fa88c368e3bca0c231bb
Ibrahim Amusan <ibamusan@gmail.com>
2020-01-24 00:24:48 +0100

    updating scores and changing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0'
@@ -30,7 +30,25 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
 
-})
+  if (dice !== 1){
+    // roundScore = roundScore + dice;
+    roundScore += dice;
+
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else{
+
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+     roundScore = 0;
+
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+  }
+
+});
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
